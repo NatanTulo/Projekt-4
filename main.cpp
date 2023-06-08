31fd78d81ddfc9276a1f8d35b285c401ef3d60eb
Mateusz Stawski <s193201@o365.student.pg.edu.pl>
2023-06-08 16:57:41 +0200

    działające okienko

--- main.cpp
+++ main.cpp
@@ -1,9 +1,123 @@
 #include <Windows.h>
 #include <gdiplus.h>
 #include <iostream>
+#include <objidl.h>
 
-int main()
+using namespace Gdiplus;
+
+void OnPaint(HDC hdc)
 {
-    std::cout << "hello world";
-    return 0;
+	Graphics graphics(hdc);
+	Pen pen(Color(255,0,0,255));
+	graphics.DrawLine(&pen,0,0,200,100);
+}
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
+{
+   HWND                hWnd;
+   MSG                 msg;
+   WNDCLASS            wndClass;
+   GdiplusStartupInput gdiplusStartupInput;
+   ULONG_PTR           gdiplusToken;
+   
+   // Initialize GDI+.
+   GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+   
+   wndClass.style          = CS_HREDRAW | CS_VREDRAW;
+   wndClass.lpfnWndProc    = WndProc;
+   wndClass.cbClsExtra     = 0;
+   wndClass.cbWndExtra     = 0;
+   wndClass.hInstance      = hInstance;
+   wndClass.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
+   wndClass.hCursor        = LoadCursor(NULL, IDC_ARROW);
+   wndClass.hbrBackground  = (HBRUSH)GetStockObject(WHITE_BRUSH);
+   wndClass.lpszMenuName   = NULL;
+   wndClass.lpszClassName  = TEXT("GettingStarted");
+   
+   RegisterClass(&wndClass);
+   
+   hWnd = CreateWindow(
+      TEXT("GettingStarted"),   // window class name
+      TEXT("Getting Started"),  // window caption
+      WS_OVERLAPPEDWINDOW,      // window style
+      CW_USEDEFAULT,            // initial x position
+      CW_USEDEFAULT,            // initial y position
+      CW_USEDEFAULT,            // initial x size
+      CW_USEDEFAULT,            // initial y size
+      NULL,                     // parent window handle
+      NULL,                     // window menu handle
+      hInstance,                // program instance handle
+      NULL);                    // creation parameters
+      
+   ShowWindow(hWnd, iCmdShow);
+   UpdateWindow(hWnd);
+   
+   while(GetMessage(&msg, NULL, 0, 0))
+   {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+   }
+   
+   GdiplusShutdown(gdiplusToken);
+   return msg.wParam;
+}
+
+/*int main()
+{
+    
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = L"SampleWindowClass";
+
+    if (!RegisterClass(&wc))
+        return 1;
+
+    HWND hwnd = CreateWindow(
+        wc.lpszClassName,
+        L"GDI+ Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
+        NULL, NULL, wc.hInstance, NULL);
+
+    if (!hwnd)
+        return 1;
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    GdiplusShutdown(gdiplusToken);
+    return static_cast<int>(msg.wParam);
+*/
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, 
+   WPARAM wParam, LPARAM lParam)
+{
+   HDC          hdc;
+   PAINTSTRUCT  ps;
+   
+   switch(message)
+   {
+   case WM_PAINT:
+      hdc = BeginPaint(hWnd, &ps);
+      OnPaint(hdc);
+      EndPaint(hWnd, &ps);
+      return 0;
+   case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+   default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+   }
 }
