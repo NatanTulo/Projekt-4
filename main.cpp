cab690a68dbcbd62933166e77bfe95de6d4a6e8d
Mateusz Stawski <s193201@o365.student.pg.edu.pl>
2023-06-11 19:16:05 +0200

    działający panel 4 guzików

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <objidl.h>
 #include <string>
 #include <map>
+using namespace Gdiplus;
 
 const int a = 255;
 const int r = 0;
@@ -12,11 +13,20 @@ const int b = 255;
 const int win_width =200; // szerokość i wysokość windy
 const int win_height = 120;
 const int win_margin = 5;
-const int BUTTON_COUNT = 5;
-int win_goal = 0;
-int win_state = 0;
+const int BUTTON_COUNT = 30;
+const float Horizontal_panel_dispersion = 1.2;
+const float Vertical_panel_dispersion = 2;
+const double Textbox_heigth_mult = 1.5;
+
 const int VELOCITY = 1;
 
+
+
+class Button_Data{
+private:
+
+
+public:
 int buttons[BUTTON_COUNT][4] ={ //x,y,width,heigth
     {},
     {},
@@ -24,9 +34,10 @@ int buttons[BUTTON_COUNT][4] ={ //x,y,width,heigth
     {},
     {},
 };
-
-
-using namespace Gdiplus;
+int win_goal = 0;
+int win_state = 0;
+};
+Button_Data data;  
 
 void drawrect(HDC hdc,int Rx, int Ry,int w, int h)
 {
@@ -53,7 +64,7 @@ void clearrect(HDC hdc,int Rx, int Ry,int w, int h)
     
 	graphics.DrawRectangle(&pen,Rx,Ry,w,h);    
 }
-void drawrectT(HDC hdc,int x,int y,const WCHAR* text,int font_value)
+Button_Data drawrectT(HDC hdc,int x,int y,const WCHAR* text,int font_value, Button_Data dt)
 {
     
     Gdiplus::Graphics    graphics(hdc);
@@ -64,17 +75,17 @@ void drawrectT(HDC hdc,int x,int y,const WCHAR* text,int font_value)
     Pen         pen(Color(a,r,g,b));
     size_t length = wcslen(text);
     graphics.DrawString(text, length, &font, pointF, &brush);
-    graphics.DrawRectangle(&pen,x,y,font_value*length,font_value*1.5);  
+    graphics.DrawRectangle(&pen,x,y,font_value*length,font_value*Textbox_heigth_mult);  
     int i = 0;
     do
     {
         
-     if(buttons[i][0]==0) 
+     if(dt.buttons[i][0]==0) 
      {
-        buttons[i][0] = x;
-        buttons[i][1] = y;
-        buttons[i][2] = font_value*length/2;
-        buttons[i][3] = font_value*1.2;
+        dt.buttons[i][0] = x;
+        dt.buttons[i][1] = y;
+        dt.buttons[i][2] = font_value*length;
+        dt.buttons[i][3] = font_value*Textbox_heigth_mult;
         i= BUTTON_COUNT;
      }   
      else
@@ -83,8 +94,21 @@ void drawrectT(HDC hdc,int x,int y,const WCHAR* text,int font_value)
      
     } while (i<BUTTON_COUNT);
     
-    
+    return dt;
 }
+Button_Data draw_panel(Button_Data dt,HDC hdc,int x,int y,const WCHAR* text1,const WCHAR* text2,const WCHAR* text3,const WCHAR* text4,int font_value)
+{
+    size_t length1 = wcslen(text1);
+    size_t length3 = wcslen(text3);
+
+    dt = drawrectT(hdc,x,y,text1,font_value,dt);
+    dt = drawrectT(hdc,x+length1*font_value*Horizontal_panel_dispersion,y,text2,font_value,dt);
+    dt = drawrectT(hdc,x,y+font_value*Vertical_panel_dispersion,text3,font_value,dt);
+    dt = drawrectT(hdc,x+length3*font_value*Horizontal_panel_dispersion,y+font_value*Vertical_panel_dispersion,text4,font_value,dt);
+    return dt;
+}
+
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
@@ -127,36 +151,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
       
    ShowWindow(hWnd, iCmdShow);
    UpdateWindow(hWnd);
-    int width;
-    int height;
-    
-    RECT rect;
-    if (GetWindowRect(hWnd, &rect))
-    {
-        width = rect.right - rect.left;
-        height = rect.bottom - rect.top;       
-    }
-    std::cout << win_state<<std::endl;
-    win_goal = height/2;
-    std::cout << win_goal<<std::endl;
 
-    if(win_state<win_goal)
-    {
-        while(win_state<win_goal)
-        {
-            win_state = win_state+VELOCITY;
-            hdc = GetDC(hWnd);
-            clearrect(hdc,width/2,win_state+1,win_width,win_height);
-            drawrect(hdc,width/2,win_state,win_width,win_height);
-            clearrect(hdc,width/2,win_state-1,win_width,win_height);
-            ReleaseDC(hWnd, hdc);
-        }
-        
-    }
     
 
 
-   
+
    while(GetMessage(&msg, NULL, 0, 0))
    {
       TranslateMessage(&msg);
@@ -166,16 +165,24 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
    GdiplusShutdown(gdiplusToken);
    return msg.wParam;
 }
-int check_coords(LPARAM lParam)
+int check_coords(LPARAM lParam, Button_Data dt)
 {
+    
     int x = LOWORD(lParam);
     int y = HIWORD(lParam);
     for(int i = 0; i<BUTTON_COUNT; ++i)
     {
-        if(buttons[i][0]<=x && buttons[i][0]+buttons[i][2]>x)
+        
+        if(dt.buttons[i][0]<=x && dt.buttons[i][0]+dt.buttons[i][2]>x)
         {
-            if(buttons[i][1]<=y && buttons[i][1]+buttons[i][3]>y)
+            std::cout<<std::endl<<dt.buttons[i][0]<<" "<<dt.buttons[i][0]+dt.buttons[i][2]<<std::endl;
+            if(dt.buttons[i][1]<=y && dt.buttons[i][1]+dt.buttons[i][3]>y)
+            {
+            std::cout<<dt.buttons[i][1]<<" "<<dt.buttons[i][1]+dt.buttons[i][3]<<std::endl;
+            std::cout<<"Button"<<std::endl;
             return i+1;
+            }
+            
             
         }           
     }
@@ -190,20 +197,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
    HDC          hdc;
    PAINTSTRUCT  ps;
    RECT rect;
+   
    int w;
    int h;
     if (GetWindowRect(hWnd, &rect))
     {
        w = rect.right - rect.left;
-       h = rect.bottom - rect.top;       
+       h = rect.bottom - rect.top;   
+         
     }
-    int buf;
+    int buf = 0;
    int wys=h-30; // do usunięcia po poprawieniu równania na h
    switch(message)
    {
     case WM_PAINT: // pierwsze pomalowanie (inicjalizujące)
-        hdc = BeginPaint(hWnd, &ps);
-        win_state = h/2;
+      
+      hdc = BeginPaint(hWnd, &ps);
+      data.win_state = h/2;
         drawrect(hdc,w/2-win_width/2,h/2,win_width,win_height); // winda
         drawrect(hdc,w/2-win_width/2-win_margin,win_margin,win_width+2*win_margin,wys-2*win_margin); // szyb windy
         drawline(hdc,0,wys/5+2*wys/3,w/2-win_width/2-win_margin,wys/5+2*wys/3,5);   // 1 piętro
@@ -211,70 +221,74 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
         drawline(hdc,0,wys/5+wys/3,w/2-win_width/2-win_margin,wys/5+wys/3,5);       // 3 piętro
         drawline(hdc,rect.right,h/3,w/2+win_width/2+win_margin,h/3,5);              // 4 piętro
         drawline(hdc,0,wys/5,w/2-win_width/2-win_margin,wys/5,5);                   // 5 piętro
-        drawrectT(hdc,w-w/4,h/4,L"Add Weight",24);
-        drawrectT(hdc,w-w/4,h/4+24*2,L"1",24);
-        drawrectT(hdc,w-w/4,h/4+24*4,L"0",24);
-        EndPaint(hWnd, &ps);
-        return 0;
+     data = draw_panel(data,hdc,w-w/4,h/4,L"1",L"1",L"1",L"1",24);
+     
+     std::cout<<std::endl;
+        for(int i = 0; i<BUTTON_COUNT;i++)
+        {
+         std::cout<<i<<" "<<data.buttons[i][0]<<" "<<data.buttons[i][1]<<" "<<data.buttons[i][2]<<" "<<data.buttons[i][3]<<std::endl;
+        }
+        std::cout<<buf<<" "<<LOWORD(lParam)<<" "<<HIWORD(lParam)<<std::endl;
+      EndPaint(hWnd, &ps);
+      return 0;
     case WM_DESTROY: // coś tam do wyłączania programu, po prostu musi być aby komputer nie wybuchnął
       PostQuitMessage(0);
       return 0;
     case WM_LBUTTONDOWN: // reakcja na wciśnięcie lewego przycisku myszy
         hdc = GetDC(hWnd);
-        buf = check_coords(lParam); // reakcja guzika, buf jest zmienną która daje ID wciśniętego guzika
-        drawrect(hdc,buttons[buf][0]+10,buttons[buf][1],buttons[buf][2],buttons[buf][3]); // tutaj na przykład wyświetla się prostokąt na koordynatach guzika jako znak, że działa
-        /* //debugging macierzy guzików
+        
+        buf = check_coords(lParam,data); // reakcja guzika, buf jest zmienną która daje ID wciśniętego guzika
+         
+         //debugging macierzy guzików
         std::cout<<std::endl;
         for(int i = 0; i<BUTTON_COUNT;i++)
         {
-         std::cout<<i<<" "<<buttons[i][0]<<" "<<buttons[i][1]<<" "<<buttons[i][2]<<" "<<buttons[i][3]<<std::endl;
+         std::cout<<i<<" "<<data.buttons[i][0]<<" "<<data.buttons[i][1]<<" "<<data.buttons[i][2]<<" "<<data.buttons[i][3]<<std::endl;
         }
         std::cout<<buf<<" "<<LOWORD(lParam)<<" "<<HIWORD(lParam)<<std::endl;
-        */
-        if(buf==2 && win_state+win_height<h-10)
+        
+        if(buf==2 && data.win_state+win_height<h-10)
         {
-            std::cout << win_goal<<std::endl;
-            win_goal= 2*wys/3+wys/5;
-            std::cout << win_goal;
+            std::cout << data.win_goal<<std::endl;
+            data.win_goal= 2*wys/3+wys/5;
+            std::cout << data.win_goal;
         }
-        if(buf==3 && win_state>10) 
+        if(buf==3 && data.win_state>10) 
         {
-            std::cout << win_goal<<std::endl;
-            win_goal= 2*h/3;
-            std::cout << win_goal<<std::endl;
-            std::cout << win_state;
+            std::cout << data.win_goal<<std::endl;
+            data.win_goal= 2*h/3;
+            std::cout << data.win_goal<<std::endl;
+            std::cout << data.win_state;
         }
 //przesuwanie windy
-        if(win_state<win_goal)
+        if(data.win_state<data.win_goal)
         {
-            while(win_state<win_goal && win_state+win_height<wys-2*win_margin)
+            while(data.win_state<data.win_goal && data.win_state+win_height<wys-2*win_margin)
             {              
                 hdc = GetDC(hWnd);
-                clearrect(hdc,w/2-win_width/2,win_state,win_width,win_height);  
-                drawrect(hdc,w/2-win_width/2,win_state+VELOCITY,win_width,win_height);           
+                clearrect(hdc,w/2-win_width/2,data.win_state,win_width,win_height);  
+                drawrect(hdc,w/2-win_width/2,data.win_state+VELOCITY,win_width,win_height);           
                 ReleaseDC(hWnd, hdc);
-                win_state = win_state+VELOCITY;
+                data.win_state = data.win_state+VELOCITY;
             }
         }
-        if(win_state>win_goal)
+        if(data.win_state>data.win_goal)
         {
             
-            while(win_state>win_goal && win_state>2*win_margin)//
+            while(data.win_state>data.win_goal && data.win_state>2*win_margin)//
             {
                       
                 hdc = GetDC(hWnd);
-                clearrect(hdc,w/2-win_width/2,win_state,win_width,win_height);  
-                drawrect(hdc,w/2-win_width/2,win_state-VELOCITY,win_width,win_height);           
+                clearrect(hdc,w/2-win_width/2,data.win_state,win_width,win_height);  
+                drawrect(hdc,w/2-win_width/2,data.win_state-VELOCITY,win_width,win_height);           
                 ReleaseDC(hWnd, hdc);
-                win_state = win_state-VELOCITY;
+                data.win_state = data.win_state-VELOCITY;
             }
         }
         ReleaseDC(hWnd, hdc);
 //przesuwanie windy
         return 0;
-    case WM_MOUSEMOVE:
-         // tu można dać coś tak myszka się rusza
-        return 0;
+    
         
    default:
       return DefWindowProc(hWnd, message, wParam, lParam);
